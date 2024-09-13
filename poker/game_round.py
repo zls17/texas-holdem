26924e58a689bea87c8d752dc0429a3f3cd01dca
zls_17 <ashishbalsure@gmail.com>
2024-09-13 22:03:48 +0530

    completed community cards logic

--- poker/game_round.py
+++ poker/game_round.py
@@ -7,7 +7,14 @@ class GameRound():
         self._shuffle_deck()
         self._deal_initial_two_cards_to_each_player()
         self._make_bets()
-        
+        self._deal_flop_cards()
+
+        self._deal_turn_card()
+        self._make_bets()
+
+        self._deal_river_card()
+        self._make_bets()
+
     def _shuffle_deck(self):
         self.deck.shuffle()
     
@@ -15,9 +22,22 @@ class GameRound():
         for player in self.players:
             two_cards = self.deck.remove_cards(2)
             player.add_cards(two_cards)
-
+    
     def _make_bets(self):
         for player in self.players:
             if player.wants_to_fold():
                 self.players.remove(player)
     
+    def _deal_community_cards(self, number):
+        community_cards = self.deck.remove_cards(number)
+        for player in self.players:
+            player.add_cards(community_cards)
+
+    def _deal_flop_cards(self):
+        self._deal_community_cards(3)
+        
+    def _deal_turn_card(self):
+        self._deal_community_cards(1)
+        
+    def _deal_river_card(self):
+        self._deal_community_cards(1)
--- tests/test_game_round.py
+++ tests/test_game_round.py
@@ -5,6 +5,32 @@ from poker.card import Card
 from poker.game_round import GameRound
 
 class GameRoundTest(unittest.TestCase):
+    def setUp(self):
+        self.first_two_cards = [
+            Card(rank = "Ace", suit = "Clubs"),
+            Card(rank = "7", suit = "Diamonds")
+        ]
+
+        self.next_two_cards = [
+            Card(rank = "King", suit = "Clubs"),
+            Card(rank = "9", suit = "Spades")
+        ]
+
+        self.flop_cards = [
+            Card("4", "Clubs"),
+            Card("Jack", "Spades"),
+            Card("Queen", "Hearts")
+        ]
+
+        self.turn_card = [
+            Card("5", "Hearts")
+        ]
+
+        self.river_card = [
+            Card("6", "Diamonds")
+        ]
+
+
     def test_stores_deck_and_players(self):
         deck = MagicMock()
         players = [
@@ -36,18 +62,14 @@ class GameRoundTest(unittest.TestCase):
         mock_deck.shuffle.assert_called_once()
 
     def test_deals_two_inital_cards_from_deck_to_each_player(self):
-        first_two_cards = [
-            Card(rank = "Ace", suit = "Clubs"),
-            Card(rank = "7", suit = "Diamonds")
-        ]
-
-        next_two_cards = [
-            Card(rank = "King", suit = "Clubs"),
-            Card(rank = "9", suit = "Spades")
-        ]
-
         mock_deck = MagicMock()
-        mock_deck.remove_cards.side_effect = [first_two_cards, next_two_cards]
+        mock_deck.remove_cards.side_effect = [
+            self.first_two_cards,
+            self.next_two_cards,
+            self.flop_cards,
+            self.turn_card,
+            self.river_card
+        ]
 
         mock_player1 = MagicMock()
         mock_player2 = MagicMock()
@@ -60,8 +82,8 @@ class GameRoundTest(unittest.TestCase):
             call(2),
             call(2)
         ])
-        mock_player1.add_cards.assert_called_with(first_two_cards)
-        mock_player2.add_cards.assert_called_with(next_two_cards)
+        mock_player1.add_cards.assert_has_calls([call(self.first_two_cards)])
+        mock_player2.add_cards.assert_has_calls([call(self.next_two_cards)])
 
     def test_removes_player_if_not_willing_to_make_bet(self):
         player1 = MagicMock()
@@ -77,6 +99,36 @@ class GameRoundTest(unittest.TestCase):
             players,
             [player2]
         )
+    
+    def test_deals_each_player_3_flop_1_turn_and_1_river_cards(self):
+        player1 = MagicMock()
+        player2 = MagicMock()
+        player1.wants_to_fold.return_value = False
+        player2.wants_to_fold.return_value = False
+        mock_deck = MagicMock()
+        mock_deck.remove_cards.side_effect = [
+            self.first_two_cards,
+            self.next_two_cards,
+            self.flop_cards,
+            self.turn_card,
+            self.river_card
+        ]
+        players = [player1, player2]
+
+        game_round = GameRound(deck = mock_deck, players = players)
+        game_round.play()
+
+        mock_deck.remove_cards.assert_has_calls([call(3), call(1), call(1)])
+
+        calls = [
+            call(self.flop_cards),
+            call(self.turn_card),
+            call(self.river_card)
+        ]
+
+        for player in players:
+            player.add_cards.assert_has_calls(calls)
+
 
 
         
